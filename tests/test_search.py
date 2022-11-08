cd9a84ee8018bd84436ec20477beb002882e4997
Adrian Debski <adr8688@sci1.jasmin.ac.uk>
2022-11-08 16:17:56 +0000

    More asset tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -62,3 +62,25 @@ def test_asset_search_within_one_item():
     res = client.search(instance_id=instance_id)
     item = next(res.items())
     assert item.get_assets() == item.assets
+
+# def test_asset_search_within_one_item_2():
+#     client = ESGFStacClient.open(API_URL)
+#     instance_id = "CMIP6.HighResMIP.MOHC.HadGEM3-GC31-HH.highres-future.r1i1p1f1.day.tas.gn.v20191105"
+#     res = client.search(instance_id=instance_id)
+#     item = next(res.items())
+#     item.
+
+
+def test_that_there_are_no_duplicates_in_item():
+    client = ESGFStacClient.open(API_URL)
+    instance_id = "CMIP6.HighResMIP.MOHC.HadGEM3-GC31-HH.highres-future.r1i1p1f1.day.tas.gn.v20191105"
+    res = client.search(instance_id=instance_id)
+    item = next(res.items())
+    assets_generator = item.get_assets()
+    file_ids_list =  [asset.properties['file_id'] for asset in assets_generator]
+    file_ids_set = set(file_ids_list)
+    # should fail, but it pass, don't know why
+    l1 = len(file_ids_list)
+    l2 = len(file_ids_set)
+    print(l1, l2)
+    assert len(file_ids_list) == len(file_ids_set)
