53d42abda28fb39770a3c430b3ec973c39ed5944
rhysrevans3 <rhys.r.evans3@gmail.com>
2023-01-18 14:56:49 +0000

    fixing pip install.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-# Git clone other repositories
-sudo git clone -b asset-search https://github.com/cedadev/pystac /workspaces/pystac
-sudo git clone -b asset-search https://github.com/cedadev/pystac-client /workspaces/pystac-client
-
-# Install Pystac requirements
-pip install -r /workspaces/pystac/requirements-dev.txt
-
-# Install Pystac-client requirements
-pip install -r  /workspaces/pystac-client/requirements-min.txt
-pip install -r /workspaces/pystac-client/requirements-dev.txt
-
-# Install Esgf-stac-client requirements
-pip install -r /workspaces/esgf-stac-client/requirements_dev.txt
-
-# Install main repositories
-pip install -e /workspaces/esgf-stac-client/. --no-deps
-pip install -e /workspaces/pystac-client/. --no-deps
-pip install -e /workspaces/pystac/. --no-deps
+# Install main repository
+perl -p -i -w -e 's/git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/g' requirements.txt
+perl -p -i -w -e 's/git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/g' requirements.txt
+pip3 install --user -e /workspaces/esgf-stac-client/. --no-deps
