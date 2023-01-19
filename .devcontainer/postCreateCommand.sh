c89c8ca9a08cdcb6b092f8dd489ab008cea719e6
rhysrevans3 <rhys.r.evans3@gmail.com>
2023-01-19 10:41:52 +0000

    using workspaceFolder instead of code-workspace.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Install main repository
-perl -p -i -w -e 's/git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/g' requirements.txt
-perl -p -i -w -e 's/git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/g' requirements.txt
+perl -p -i -w -e 's/git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/g' esgf-stac-client/requirements.txt
+perl -p -i -w -e 's/git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/g' esgf-stac-client/requirements.txt
 pip3 install --user -e /workspaces/esgf-stac-client/. --no-deps
