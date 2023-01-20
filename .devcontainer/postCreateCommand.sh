063056cbe09902026f686c40bbde1ddaf5eac436
Ag Stephens <ag.stephens@stfc.ac.uk>
2023-01-20 18:59:05 +0000

    Updated postCreateCommand: to update pyopenssl which caused pytest failures

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,4 +3,6 @@
 # Install main repository
 perl -p -i -w -e 's/^git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/g' esgf-stac-client/requirements.txt
 perl -p -i -w -e 's/^git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/g' esgf-stac-client/requirements.txt
-pip3 install --user -e /workspaces/esgf-stac-client/. --no-deps
+pip install --user -e /workspaces/esgf-stac-client/. --no-deps
+pip install --user pip --upgrade
+pip install --user pyopenssl --upgrade
