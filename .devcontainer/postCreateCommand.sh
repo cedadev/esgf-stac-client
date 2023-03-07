e8f9feb7cd58794c749d831a38c96064fe846809
rhysrevans3 <rhys.r.evans3@gmail.com>
2023-03-07 09:32:31 +0000

    Adding local elasticsearch for devcontainer.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 # Install main repository
-perl -p -i -w -e 's/^git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac.git\@asset-search/g' esgf-stac-client/requirements.txt
-perl -p -i -w -e 's/^git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/#git\+https:\/\/github.com\/cedadev\/pystac-client.git\@asset-search/g' esgf-stac-client/requirements.txt
-pip3 install --user -e /workspaces/esgf-stac-client/. --no-deps
-
-cd /workspaces/stac-fastapi-elasticsearch
-export STAC_ELASTICSEARCH_SETTINGS=stac_fastapi.elasticsearch.settings
-uvicorn stac_fastapi.elasticsearch.app:app --reload
+cd /workspaces/esgf-stac-client/
+pip3 install --user -r requirements.txt
+pip3 install --user -e . --no-deps
